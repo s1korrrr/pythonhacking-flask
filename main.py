25354464ef917fce1cb78ed4c83cf58cf41767ac
n3st0r <piotr.dobrysiak@gmail.com>
2017-05-11 22:18:33 +0200

    Warsztaty 2, praca z formularzem.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import time
-
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return render_template('index.html', name=request.form['name'])
+    else:
+        return render_template('index.html')
 
 
 @app.route('/some_json')
@@ -24,3 +26,14 @@ def some_json():
 def hackerspace(number):
     l = [x**3 for x in range(1, number+1)]
     return str(l)
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def formularz():
+    if request.method == 'POST':
+        return render_template('site.html', name=request.form['name'])
+    if request.method == 'GET':
+        return render_template('form.html')
+
+if __name__ == "__main__":
+    app.run()
