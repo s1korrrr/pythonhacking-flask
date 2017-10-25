c389499d58868c55e0076b8b99662c80972bb1db
s1korrrr <s1korrrr@gmail.com>
2017-10-25 19:28:24 +0200

    flake8 added

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object('src.default_settings')
+if 'LOCAL_SETTINGS' in os.environ:
+    app.config.from_envvar('LOCAL_SETTINGS')
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
+login_manager.init_app(app)
---
+++ src/default_settings.py
@@ -0,0 +1,15 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = False
+TESTING = False
+
+DATABASE_URI = 'sqlite:///car_club.db'
+SECRET_KEY = ('''
+    \xb7\x06\xca\xbe\x8f\xda\xf9\x95A\tY+\x1as\xa7\xc7\x11;\xa9i\xc48\xb5o\
+''')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,
+                                                      '..', 'car_club.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
